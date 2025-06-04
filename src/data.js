3b89c07cb842577413b66ede0c7de43672c31a88
SharkCtts <149117514+SharkCtts@users.noreply.github.com>
2025-06-03 19:08:13 -0500

    organizacion de los modelos y como se ven en pantalla
    agregue descripción

--- src/data.js
+++ src/data.js
@@ -3,24 +3,28 @@ export const productos = [
       id: 1,
       nombre: "Silla Oficina",
       precio: 300000,
+      descripcion: "una silla lol",
       modelo: "/model/chair.glb",
     },
     {
       id: 2,
       nombre: "Escritorio",
       precio: 500000,
+      descripcion: "un escritorio que más",
       modelo: "/model/desk.glb",
     },
     {
       id: 3,
       nombre: "Lampara de piso",
       precio: 120000,
+      descripcion: "la lampara de toy story",
       modelo: "/model/lamp.glb",
     },
     {
         id: 3,
         nombre: "Puerta de Madera",
         precio: 80000,
+        descripcion: "una puerta no way",
         modelo: "/model/door.glb",
       },
   ];
